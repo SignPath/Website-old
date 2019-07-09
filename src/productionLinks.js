a0030622e59ea69c7a02955c573c77614262a59c
Paul Savoie <paul.savoie@rubicon.eu>
2019-07-09 13:19:40 +0200

    fixes broken links

--- src/productionLinks.js
+++ src/productionLinks.js
@@ -6,7 +6,7 @@ export default {
 	'code-signing/3_windows':                           '/code-signing/windows-platform',
     'documentation/1_introduction.md.html':             '/documentation',
     'documentation/2_key_concepts.md.html':             '/documentation/key-concepts',
-    'documentation/3_artifact_configuration.md.html':   '/documentation/creating-an-artifact-configuration',
+    'documentation/3_artifact_configuration.md.html':   '/documentation/artifact-configuration',
     'documentation/4_build_integration.md.html':        '/documentation/build-system-integration'
 }
 
